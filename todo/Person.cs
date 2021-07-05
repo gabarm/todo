12ade2347ffcc62346d244e2221f59b70821c146
Gabriel Barmelki <klicky91@gmail.com>
2021-07-05 02:06:01 +0200

    Add project files.

---
+++ todo/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class Person
+    {
+        public string person;
+        static int idCounter;
+        public static int counter;
+
+    }
+}
